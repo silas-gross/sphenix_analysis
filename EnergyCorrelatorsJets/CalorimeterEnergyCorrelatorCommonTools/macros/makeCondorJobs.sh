3826f4322aac5098bafdd410a3a03e7f8ccaa621
Silas K Grossberndt <silas.grossberndt@gmail.com>
2026-04-30 17:19:25 -0400

    some minor updates

--- EnergyCorrelatorsJets/CalorimeterEnergyCorrelatorCommonTools/macros/makeCondorJobs.sh
+++ EnergyCorrelatorsJets/CalorimeterEnergyCorrelatorCommonTools/macros/makeCondorJobs.sh
@@ -50,7 +50,7 @@ make_truth_condor_jobs()
 		IFS=$'\n' read -d '' -r -a blanklines < $condor_testfile
 		echo "${blanklines[0]}" > $condor_file 
 		echo "${blanklines[1]}"$(pwd)"/${executable}" >> $condor_file
-		echo "${blanklines[2]}" $(pwd) ${options} ${MYINSTALL} >> $condor_file
+		echo "${blanklines[2]}" $(pwd) ${options} ${outDir} $MYINSTALL >> $condor_file
 		echo "${blanklines[3]}"$condor_out_file >> $condor_file
 		echo "${blanklines[4]}"$condor_err_file >> $condor_file
 		echo "${blanklines[5]}"$condor_log_file >> $condor_file
